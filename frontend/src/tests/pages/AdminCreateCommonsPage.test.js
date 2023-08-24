d4a6f36492e506355623c9450d76e7c4cd80bc31
Joshua Zhou <nov3mber257@gmail.com>
2023-08-23 18:36:35 -0700

    JZ - fixed file that was merged into main to account for change in startingDate form position

--- frontend/src/tests/pages/AdminCreateCommonsPage.test.js
+++ frontend/src/tests/pages/AdminCreateCommonsPage.test.js
@@ -117,8 +117,8 @@ describe("AdminCreateCommonsPage tests", () => {
         const expectedCommons = {
             name: "My New Commons",
             startingBalance: 500,
-            cowPrice: 10,
             milkPrice: 5,
+            cowPrice: 10,
             degradationRate: 30.4,
             carryingCapacity: 25,
             startingDate: '2022-03-05T00:00:00.000Z',  // [1]
